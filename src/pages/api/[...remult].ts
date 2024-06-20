d190d7ba1d2abb6c4755ba306e155dcb557cf280
Elena <ewlen04@gmail.com>
2024-06-20 12:26:25 +0200

    connection to superbase and variables setting

--- src/pages/api/[...remult].ts
+++ src/pages/api/[...remult].ts
@@ -2,9 +2,11 @@ import { remultNext } from "remult/remult-next";
 import { Product } from "../../shared/Product";
 import { ProductsController } from "@/shared/ProductsController";
 import { getUserFromNextAuth } from "./auth/[...nextauth]";
+import { createPostgresConnection } from "remult/postgres";
 
 export default remultNext ({
     controllers: [ProductsController],
     entities: [Product],
-    getUser: getUserFromNextAuth
+    getUser: getUserFromNextAuth, 
+    dataProvider: createPostgresConnection()
 })
