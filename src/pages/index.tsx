f46be3a6a9f72cac69333cd5490f8b4c9b6cf818
Elena <ewlen04@gmail.com>
2024-06-24 12:09:08 +0200

    design  modifs25

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -113,7 +113,7 @@ export default function Home() {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="gold" className="size-6">
                 <defs>
                 <pattern id="imagePattern" patternUnits="userSpaceOnUse" width="24" height="24">
-                  <image href="/pic1.jpg" x="0" y="0" width="24" height="24" />
+                  <image href="/backgroundBtn1.png" x="0" y="0" width="24" height="24" />
                 </pattern>
               </defs>
                   <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
