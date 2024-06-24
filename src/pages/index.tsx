083d8b5ab448db6160543c34cfab0887c631661b
Elena <ewlen04@gmail.com>
2024-06-24 15:32:53 +0200

    design  modifs29

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
   return (
     <div className="bg-picMain h-screen flex items-center flex-col justify-center text-lg bg-cover">
       <h1 className="text-gold text-8xl italic bold shadow-lg mb-2">Shopping list {products.length} </h1>
-      <main className="bg-white border rounded-lg shadow-lg m-15 w-screen max-w-md">
+      <main className="bg-white border rounded-2xl shadow-lg m-15 w-screen max-w-md">
         <div className="flex justify-between px-15 p-5 border-b">
           Welcome to our website, {session?.user?.name}{" "}
           <div className="gap-4 flex justify-between">
