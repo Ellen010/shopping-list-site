3e285f97891fe84096043aaa1413e5796ee64c21
Elena <ewlen04@gmail.com>
2024-06-24 11:53:00 +0200

    design  modifs19

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home() {
       <main className="bg-white border rounded-lg shadow-lg m-5 w-screen max-w-md">
         <div className="flex justify-between px-6 p-10 border-b">
           Welcome to our website, {session?.user?.name}{" "}
-          <div className="border-t px-6 p-2 gap-4 flex justify-between">
+          <div className="gap-4 flex justify-between">
           <button 
           className="text-white px-3 py-1 font-semibold rounded-lg mb-2"
           style={{
